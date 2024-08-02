c5fa5f112d615d2e7b228493e833de16854eb9d6
Rohit Kalyani <rohitkalyani@Rohits-MacBook-Air.local>
2024-08-03 01:07:49 +0530

    code is completed

---
+++ new1/new1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  new1
+//
+//  Created by Rohit Kalyani on 03/08/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
